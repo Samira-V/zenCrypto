b4d3673e9bcfe765fea861786776ea44c7135ac0
samira vaeidi <samiraa.vaeidi@gmail.com>
2025-09-08 14:50:24 +0330

    responsive-1

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let acc = document.getElementsByClassName("accordion");
+console.log(acc)
 let i;
-let plus = document.querySelector('.plus')
-let minus = document.querySelector('.minus')
+// let plus = document.querySelector('.plus')
+// let minus = document.querySelector('.minus')
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
 
     this.classList.toggle("active");
-    this.style.padding = "10px";
+    // this.style.padding = "10px";
    
-     
-
-
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight === "150px") {
       panel.style.maxHeight = "0";
@@ -21,4 +19,15 @@ for (i = 0; i < acc.length; i++) {
     
     }
   });
+}
+let all = document.getElementsByTagName("*");
+ let j = 0;
+  let rect
+  let docWidth = document.documentElement.offsetWidth;
+for (; j < all.length; j++) {
+    rect = all[i].getBoundingClientRect();
+    if (rect.right > docWidth || rect.left < 0){
+        console.log(all[i]);
+        all[i].style.borderTop = '1px solid red';
+    }
 }
