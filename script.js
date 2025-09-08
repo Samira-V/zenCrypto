2b4d4be987befa676982df0f8cd7db7edfbc36e8
samira vaeidi <samiraa.vaeidi@gmail.com>
2025-09-08 13:24:15 +0330

    footer update

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ for (i = 0; i < acc.length; i++) {
 
 
     let panel = this.nextElementSibling;
-    if (panel.style.height === "150px") {
-      panel.style.height = "0";
+    if (panel.style.maxHeight === "150px") {
+      panel.style.maxHeight = "0";
    
     } else {
-      panel.style.height = "150px";
+      panel.style.maxHeight = "150px";
     
     }
   });
