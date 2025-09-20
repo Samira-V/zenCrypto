7e634a5064b6fef71e34869bb1ed3e5768e9b399
samira vaeidi <samiraa.vaeidi@gmail.com>
2025-09-20 17:55:34 +0330

    update mobile menu

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 let acc = document.getElementsByClassName("accordion");
 let menu = document.querySelector(".mobile-menu-container")
 let header = document.querySelector(".header")
-
+let burger = document.querySelector(".burger")
+const cursor = document.querySelector(".cursor")
+let menuItem = document.querySelectorAll(".menu-item")
 let i;
-// let plus = document.querySelector('.plus')
-// let minus = document.querySelector('.minus')
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
 
     this.classList.toggle("active");
     // this.style.padding = "10px";
-   
+
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight === "150px") {
       panel.style.maxHeight = "0";
-   
+
     } else {
       panel.style.maxHeight = "150px";
-    
+
     }
   });
 }
 let all = document.getElementsByTagName("*");
- let j = 0;
-  let rect
-  let docWidth = document.documentElement.offsetWidth;
+let j = 0;
+let rect
+let docWidth = document.documentElement.offsetWidth;
 for (; j < all.length; j++) {
-    rect = all[i].getBoundingClientRect();
-    if (rect.right > docWidth || rect.left < 0){
-        console.log(all[i]);
-        all[i].style.borderTop = '1px solid red';
-    }
+  rect = all[i].getBoundingClientRect();
+  if (rect.right > docWidth || rect.left < 0) {
+    console.log(all[i]);
+    all[i].style.borderTop = '1px solid red';
+  }
 }
- menu.addEventListener('click' , ()=>{
+
+burger.addEventListener('click', () => {
   header.classList.toggle('openMenu')
- })
- const cursor = document.querySelector(".cursor")
- document.addEventListener("mousemove",(e)=>{
-cursor.style.left = `${e.clientX}px`
-cursor.style.top = `${e.clientY}px`
- })
+})
+
+menu.addEventListener("mousemove", (e) => {
+  console.log(e)
+  cursor.style.left = `${e.clientX}px`
+    cursor.style.top = `${e.clientY}px`
+})
+
+for (item of menuItem) {
+  item.addEventListener('mouseenter', () => {
+    cursor.style.opacity = "1"
+    cursor.style.transform = "translate(-50% , -50%) scale(1)"
+  })
+}
+for (item of menuItem) {
+  item.addEventListener('mouseleave', () => {
+    cursor.style.opacity = "0"
+    cursor.style.transform = "translate(-50% , -50%) scale(0)"
+  })
+}
